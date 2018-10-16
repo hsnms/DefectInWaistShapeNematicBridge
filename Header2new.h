8d3c2e1c3a9a54e3baaab183162fe8d3f2371d4e
Shengnan Huang <hsnms1226@gmail.com>
2018-10-15 20:09:04 -0400

    first attempt

---
+++ Header2new.h
@@ -0,0 +1,12 @@
+#ifndef HEADER2NEW_H
+#define HEADER2NEW_H
+double *dvectornew(long nl, long nh);
+int  *vectornew(long nl, long nh);
+/*double *dvector2new(long nl, long nh);*/
+double **dmatrix2new(long nrl,long nrh,long ncl,long nch);
+void free_dmatrix2new(double **m, long nrl, long nrh, long ncl, long nch);
+void fill02new(double **u, int n, double m, int a);
+void free_dvectornew(double *v, long nl, long nh);
+void free_vectornew(int *v, long nl, long nh);
+/*void free_dvector2new(double *v, long nl, long nh);*/
+#endif
